7a9341a5f7a754ba44dfb8ef6e9ccc5f881e1291
David Dyer <davidrossdyer@gmail.com>
2021-12-02 13:50:09 -0500

    separated login/signup formhandlers

--- public/js/login.js
+++ public/js/login.js
@@ -22,29 +22,4 @@ const loginFormHandler = async (event) => {
   }
 };
 
-const signupFormHandler = async (event) => {
-  event.preventDefault();
-
-  const username = document.querySelector("#username-signup").value.trim();
-  const password = document.querySelector("#password-signup").value.trim();
-
-  if (username && password) {
-    const response = await fetch("/api/users", {
-      method: "POST",
-      body: JSON.stringify({ username, password }),
-      headers: { "Content-Type": "application/json" },
-    });
-
-    if (response.ok) {
-      document.location.replace("/dash");
-    } else {
-      alert(response.statusText);
-    }
-  }
-};
-
 document.querySelector("#loginBtn").addEventListener("click", loginFormHandler);
-
-document
-  .querySelector("#signupBtn")
-  .addEventListener("click", signupFormHandler);
