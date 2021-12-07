32cd6be5ca767110319630e2fb93ab7ff243cf14
David Dyer <davidrossdyer@gmail.com>
2021-12-06 22:55:52 -0500

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -3,16 +3,32 @@ const Sequelize = require("sequelize");
 // Enable access to .env variables
 require("dotenv").config();
 
-// Use environment variables to connect to database
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PW,
-  {
-    host: "localhost",
-    dialect: "mysql",
-    port: 3306,
-  }
-);
+// // Use environment variables to connect to database
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PW,
+//   {
+//     host: "localhost",
+//     dialect: "mysql",
+//     port: 3306,
+//   }
+// );
 
-module.exports = sequelize;
+// module.exports = sequelize;
+
+let sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
