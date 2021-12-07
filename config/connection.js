9c7ad90ce022b5a3f7955f97d05b4358cf3f692f
David Dyer <davidrossdyer@gmail.com>
2021-12-06 23:49:22 -0500

    changed connetion again

--- config/connection.js
+++ config/connection.js
@@ -3,20 +3,6 @@ const Sequelize = require("sequelize");
 // Enable access to .env variables
 require("dotenv").config();
 
-// // Use environment variables to connect to database
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PW,
-//   {
-//     host: "localhost",
-//     dialect: "mysql",
-//     port: 3306,
-//   }
-// );
-
-// module.exports = sequelize;
-
 let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
@@ -32,3 +18,5 @@ if (process.env.JAWSDB_URL) {
     }
   );
 }
+
+module.exports = sequelize;
